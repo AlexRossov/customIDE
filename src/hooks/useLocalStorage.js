e3a5b05e417e043c009eab824904004e30f588d2
Alex Rossov <poccob86@gmail.com>
2024-12-19 15:38:47 +0800

    fix if key localStorage = null

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,9 +3,10 @@ import { useEffect, useState } from 'react';
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      return typeof window !== 'undefined'
-        ? window.localStorage.getItem(key)
-        : initialValue;
+      if (typeof window === 'undefined') return initialValue;
+      const item = window.localStorage.getItem(key);
+      // Если ключ отсутствует, используем initialValue
+      return item !== null ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.error(error);
       return initialValue;
@@ -15,11 +16,15 @@ export const useLocalStorage = (key, initialValue) => {
   // Отложенное сохранение с debounce 300 мс
   useEffect(() => {
     const handler = setTimeout(() => {
-      window.localStorage.setItem(
-        key,
-        typeof storedValue === 'string' ? storedValue : JSON.stringify(storedValue)
-      );
+      try {
+        if (typeof window !== 'undefined') {
+          window.localStorage.setItem(key, JSON.stringify(storedValue));
+        }
+      } catch (error) {
+        console.error('Ошибка при сохранении в localStorage:', error);
+      }
     }, 300);
+
 
     return () => clearTimeout(handler); // Очищаем таймер при каждом изменении storedValue
   }, [storedValue, key]);
